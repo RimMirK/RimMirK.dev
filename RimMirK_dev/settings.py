ae220770fc6e829a207397c730cafa3dd3b2c481
RimMirK <github@rimmirk.dev>
2026-03-22 13:10:34 +0100

    Update Django settings with new configurations

--- RimMirK_dev/settings.py
+++ RimMirK_dev/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "RimMirK_dev.middlewares.Notify404Middleware",
@@ -124,6 +125,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.environ["STATIC_ROOT"]
 STATICFILES_DIRS = []
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
